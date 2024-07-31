6a3274999229b58309e4dd7580d32f29629c462c
Resul Elezi <resul_elezi@protonmail.com>
2024-07-31 16:38:36 +0200

    #

---
+++ script.js
@@ -0,0 +1,5 @@
+"use strict";
+
+const taskForm = document.getElementById("task-form");
+const confirmCloseDialog = document.getElementById("confirm-close-dialog");
+const openTaskFormBtn = document.getElementById("open-task-form-btn");
