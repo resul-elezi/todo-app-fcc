2c80e16cb05e0fb25fa7bb7482e878582e277139
Resul Elezi <resul_elezi@protonmail.com>
2024-07-31 17:31:10 +0200

    #

--- script.js
+++ script.js
@@ -29,3 +29,26 @@ discardBtn.addEventListener("click", () => {
     confirmCloseDialog.close();
     taskForm.classList.toggle("hidden");
   });
+
+  taskForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+  
+    const dataArrIndex = taskData.findIndex((item) => item.id === currentTask.id);
+    const taskObj = {
+      id: `${titleInput.value.toLowerCase().split(" ").join("-")}-${Date.now()}`,
+      title: titleInput.value,
+      date: dateInput.value,
+      description: descriptionInput.value,
+    };
+  
+     if (dataArrIndex === -1) {
+      taskData.unshift(taskObj);
+    }
+  
+    taskData.forEach(({id, title, date, description}) => {
+        (tasksContainer.innerHTML += `
+            
+    `)
+    }
+  );
+});
