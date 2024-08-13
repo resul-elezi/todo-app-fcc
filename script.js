1539611f18b2f6381935534351732b42383b6427
Resul Elezi <resul_elezi@protonmail.com>
2024-08-13 12:07:31 +0200

    #

--- script.js
+++ script.js
@@ -46,7 +46,9 @@ discardBtn.addEventListener("click", () => {
   
     taskData.forEach(({id, title, date, description}) => {
         (tasksContainer.innerHTML += `
-            
+           <div class="task" id="${id}">
+            <p><strong>Title:</strong> ${title}</p>
+           </div> 
     `)
     }
   );
