407a1b338edce325449f24b23a060f70ba1d0cc1
Resul Elezi <resul_elezi@protonmail.com>
2024-08-13 19:14:55 +0200

    #

--- script.js
+++ script.js
@@ -15,6 +15,14 @@ const descriptionInput = document.getElementById("description-input");
 const taskData = [];
 let currentTask = {};
 
+const reset = () => {
+  titleInput.value = "";
+  dateInput.value = "";
+  descriptionInput.value = "";
+  taskForm.classList.toggle("hidden");
+  currentTask = {};
+}
+
 openTaskFormBtn.addEventListener("click", () =>
     taskForm.classList.toggle("hidden")
 );
