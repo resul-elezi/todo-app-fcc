4b639f8c81ef4508e6aff7a61dcacb04c07e0f42
Resul Elezi <resul_elezi@protonmail.com>
2024-08-20 23:51:24 +0200

    Update script.js

--- script.js
+++ script.js
@@ -51,7 +51,6 @@ const updateTaskContainer = () => {
   );
 };
 
-
 const deleteTask = (buttonEl) => {
   const dataArrIndex = taskData.findIndex(
     (item) => item.id === buttonEl.parentElement.id
